86a01da8d733985428ffa0898327fba83beb105a
SAJID 23.0 <mdsajid1375@gmail.com>
2024-12-03 23:29:37 +0600

    home

--- src/app/components/Home/Banner.js
+++ src/app/components/Home/Banner.js
@@ -5,6 +5,153 @@ const Banner = () => {
     <section className='py-56 bg-[url("/banner.jpg")] bg-no-repeat bg-cover bg-center '>
         <div className='container'>
             <h1 className='text-5xl text-white text-center'>this is home page</h1>
+            <p>About
+HTML
+CSS
+JavaScript
+More
+Blog
+Learn
+Explore
+Patterns
+Case studies
+Search
+/
+
+
+Language
+Sign in
+Guidance to build modern web experiences that work on any browser.
+
+Building a better web, together
+We want to help you build beautiful, accessible, fast, and secure websites that work cross-browser, and for all of your users. This site is our home for content to help you on that journey, written by members of the Chrome team, and external experts who specialize in web development topics such as accessibility, performance, design, and more.
+
+Baseline
+Web Platform Baseline brings clarity to information about browser support for web platform features, providing clear information about which web platform features are ready to use in your projects today. When reading an article here on web.dev, if the features used are all part of Baseline, you can trust the level of browser compatibility.
+Learn more
+
+AI and the web
+Discover resources created to help you understand and build performant, web-first experiences with AI.
+Learn more
+
+Optimize INP
+Interaction to Next Paint (INP) is now a Core Web Vital metrics. Start measuring and optimizing your site's INP.
+Learn more
+
+Payments & Identity
+Read up on the latest features and best practices when it comes to identity and payment processing.
+Learn more
+Explore the web platform
+The web platform is a constantly evolving ecosystem of browser features and APIs that developers can use to make websites that are easy and delightful to use. These pages are pathways for you to dive into each part of the web platform on web.dev—at your pace:
+image
+HTML
+HTML is the document layer of the web, providing structure and semantics for pages.
+Learn more
+web
+CSS
+CSS is the presentation layer of the web, and allows you to your web pages look just like you want them to.
+Learn more
+timer
+JavaScript
+JavaScript provides rich interactivity to web pages, allowing developers to build richly interactive websites that help users to perform tasks and accomplish goals.
+Learn more
+Learn the web platform
+New to the web platform? We've got you covered! Dive into each part of the web platform with these courses curated specifically for beginners to HTML, CSS, and JavaScript.
+
+Course
+Learn HTML
+A solid overview of HTML for developers, from novice to expert level HTML.
+Start course
+
+Course
+Learn CSS
+A guide to CSS with modules covering everything from accessibility to z-index.
+Start course
+
+Course
+Learn JavaScript
+An in-depth course on the basics of JavaScript.
+Start course
+
+Keep up on web development news
+Our blog contains the latest news in web development, including new web platform features, updates to Baseline, AI, Core Web Vitals, and more. Keep up with the latest blog reading our blog.
+Read the blog
+CSS and UI design
+Learn the latest tips and techniques to use in your work, right now.
+Explore CSS
+Adapting typography to user preference using CSS
+Adapt a font to your users' preferences, so they're maximally comfortable reading your content.
+New CSS color spaces and functions in all major engines
+All major engines now support the new CSS color spaces and functions. Find out how they can bring vibrancy to your designs.
+What are source maps?
+Improve web debugging experience with source maps.
+CSS subgrid
+Subgrid is now interoperable across all three major engines. Find out how to use it.
+Core Web Vitals
+Build faster websites and apps by understanding Core Web Vitals and other metrics.
+Explore Core Web Vitals
+Interaction to Next Paint becomes a Core Web Vital on March 12
+Interaction to Next Paint will officially become a Core Web Vital and will replace First Input Delay on March 12.
+How to optimize INP
+Learn how to optimize for the Interaction to Next Paint metric.
+Our top Core Web Vitals recommendations
+A collection of best practices for optimizing websites' Core Web Vitals performance based on the state of the web
+Common misconceptions about how to optimize LCP
+Explore common misconceptions about optimizing Largest Contentful Paint (LCP) by looking beyond image optimization and considering factors like Time to First Byte (TTFB) and resource load delay.
+Progressive Web Apps
+Create capable web experiences.
+More on PWA
+Learn PWA
+A course that breaks down every aspect of modern progressive web app development.
+TransformStream is now supported cross browser.
+Now that transform streams are supported in Safari, Firefox, and Chrome they are finally ready for prime time.
+New patterns for amazing apps
+Dive into a fantastic collection of new patterns for amazing apps, including clipboard patterns, file patterns, and advanced app patterns.
+Project Fugu API showcase
+On the Chrome Developers site, explore capabilities APIs.
+Accessible for all
+Learn how to make your sites more accessible for people with disabilities, whether permanent, temporary, or situational.
+More on accessibility
+Learn Accessibility
+Our new course is a great entry point and reference for accessibility topics.
+Testing web design color contrast
+An overview of three tools and techniques for testing and verifying accessible color contrast of your design.
+Building the main navigation for a website
+This tutorial describes how to build an accessible main navigation of a website. You learn about semantic HTML, accessibility, and how using ARIA attributes can sometimes do more harm than good.
+Community highlight: Melanie Sumner
+Read our interview with Melanie, a software engineer who specializes in digital accessibility.
+Payments and identity
+Find the latest techniques and best practices for payments and helping users safely log into your sites and apps.
+More on identity
+Create a passkey for passwordless logins
+Passkeys make user accounts safer, simpler, easier to use.
+Sign in with a passkey
+Create a sign in experience that uses passkeys while still accommodating existing password users.
+Web payments overview
+Learn more about Web Payments and how they work.
+Payment and address form best practices
+Maximize conversions by helping your users complete address and payment forms as quickly and easily as possible.
+Developer Newsletter
+Get the latest news, techniques and updates straight to your inbox.
+
+We want to help you build beautiful, accessible, fast, and secure websites that work cross-browser, and for all of your users. This site is our home for content to help you on that journey, written by members of the Chrome team, and external experts.
+Contribute
+File a bug
+See open issues
+Related Content
+Chrome for Developers
+Chromium updates
+Case studies
+Podcasts & shows
+Follow
+@ChromiumDev on X
+YouTube
+Chrome for Developers on LinkedIn
+RSS
+Terms
+Privacy
+
+Language</p>
         </div>
     </section>
   )
